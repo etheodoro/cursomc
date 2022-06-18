1bee28b33fb2df6cda17b4b5666c7e666aa0a67a
Evandro Theodoro <evandrolt@gmail.com>
2022-06-18 15:30:58 -0300

    Produto associação muitos para muitos

--- src/main/java/com/etheodoro/cursomc/CursomcApplication.java
+++ src/main/java/com/etheodoro/cursomc/CursomcApplication.java
@@ -1,8 +1,6 @@
 package com.etheodoro.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.etheodoro.cursomc.domain.Categoria;
+import com.etheodoro.cursomc.domain.Produto;
 import com.etheodoro.cursomc.repositories.CategoriaRepository;
-import com.etheodoro.cursomc.repository.CategoriaRepositories;
+import com.etheodoro.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,7 +30,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		repo.saveAll(Arrays.asList(cat1,cat2));		
+		Produto p1 = new Produto(null, "Computador", 2000D);
+		Produto p2 = new Produto(null, "Impressoara", 800D);
+		Produto p3 = new Produto(null, "Mouse", 80D);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 }
